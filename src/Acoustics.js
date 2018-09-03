dc014407b1cfa1d64e6ce822c6ac47994a25af08
lvnr <levonarakelian@live.com>
2018-09-03 16:42:52 +0400

    initial implementation [WIP]

---
+++ src/Acoustics.js
@@ -0,0 +1,87 @@
+import _ from 'lodash'
+
+const format = (num) => Math.round(num)
+
+const K_FACTOR = 0.161
+
+class Acoustics {
+  static RT60 = (V, A) => K_FACTOR * V / A
+
+  static A = (V, T) => K_FACTOR * V / T
+
+  static A_eq = (alpha, S) => alpha * S
+
+  static alpha = (A_eq, S) => A_eq / S
+
+  static surfaceAreas = (dimensions) => {
+    const width = dimensions.width / 100
+    const height = dimensions.height / 100
+    const length = dimensions.length / 100
+
+    if (!width || !length || !height) return {}
+
+    const ceiling = width * length
+    const floor = ceiling
+    const walls = (width * height * 2) + (length * height * 2)
+    const total = ceiling + floor + walls
+    const results = { ceiling, floor, walls, total }
+    _.each(results, (num, key) => results[key] = _.round(num, 2))
+
+    return results
+  }
+
+  static volume = (dimensions) => {
+    const width = dimensions.width / 100
+    const height = dimensions.height / 100
+    const length = dimensions.length / 100
+
+    if (!width || !length || !height) return null
+
+    return (width * length * height).toFixed(2)
+  }
+
+  // Reverberation time RT60 = 0.161 · V / A (V and A in meter)
+  // A0 = 0.161 · V / RT60
+  static A_eqs = (RT60Data, V) => {
+    if (_.isEmpty(RT60Data) || !V) return {}
+
+    let A_eq = {}
+
+    _.each(RT60Data, data => {
+      const { frequency, RT60: T } = data
+      const A = Acoustics.A(V, T)
+      A_eq[frequency] = _.round(A, 2)
+    })
+
+    return A_eq
+  }
+
+  static alphas = (A_eq, S) => {
+    if (_.isEmpty(A_eq) || !S) return {}
+
+    let alpha = {}
+
+    _.each(A_eq, (A, hz) => {
+      alpha[hz] = _.round(A / S, 3)
+    })
+
+    return alpha
+  }
+
+  static A_adds = (A_eqs, RT60s, alphas, T_opt, V) => {
+    if (_.isEmpty(A_eqs) || _.isEmpty(alphas) || _.isEmpty(RT60s) || !T_opt) return {}
+
+    let A_adds = {}
+
+    _.each(RT60s, ({ frequency, RT60: T }) => {
+      const A_0 = A_eqs[frequency]
+      const A_req = Acoustics.A(V, T_opt)
+      const A_add = A_req - A_0
+      A_adds[frequency] = _.round(A_add)
+    })
+
+    return A_adds
+  }
+}
+
+export default Acoustics
--- src/App.js
+++ src/App.js
@@ -1,21 +1,240 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Layout, Row, Col, Input, InputNumber, Radio, Switch, Icon } from 'antd'
+import { Persist } from 'react-persist'
+import { VictoryArea, VictoryChart, VictoryAxis } from 'victory'
+import Acoustics from './Acoustics'
+import './App.css'
+
+const { Content } = Layout
+
+const FrequencyDomain = [63, 125, 250, 500, 1000, 2000, 4000, 8000]
 
 class App extends Component {
+  state = {
+    length: 600,
+    width: 400,
+    height: 310,
+    RT60Target: null,
+    customRT60Target: true,
+    measuredRT60: { 63: null, 125: null, 250: null, 500: null, 1000: null, 2000: null, 4000: null, 8000: null },
+  }
+
+  handleCustomRT60Target = (checked) => {
+    this.setState({ customRT60Target: checked })
+  }
+
+  handleCustomRT60TargetInput = (value) => {
+    this.setState({ RT60Target: value })
+  }
+
+  handleInput = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleMeasuredRT60Input = (e) => {
+    this.setState({ measuredRT60: { ...this.state.measuredRT60, [e.target.name]: e.target.value } })
+  }
+
   render() {
+    const { width, length, height } = this.state
+    const dimensions = { width, length, height }
+
+    const surfaceAreas = Acoustics.surfaceAreas(dimensions)
+    const volume = Acoustics.volume(dimensions)
+    const measuredRT60Data = FrequencyDomain.map(hz => ({ frequency: hz, RT60: Number(this.state.measuredRT60[hz]) }))
+    const A_eqs = Acoustics.A_eqs(measuredRT60Data, volume)
+    const alphas = Acoustics.alphas(A_eqs, surfaceAreas.total)
+    const A_adds = Acoustics.A_adds(A_eqs, measuredRT60Data, alphas, this.state['RT60Target'], volume)
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Persist
+          name="room-data"
+          data={this.state}
+          debounce={500}
+          onMount={data => this.setState(data)}
+        />
+
+        <Row type="flex" gutter={16} style={{ padding: '32px 32px 0 32px' }}>
+          <span>Room Dimensions</span>
+        </Row>
+
+        <Row type="flex" gutter={16} style={{ padding: '32px' }}>
+          <Col span={6}>
+            <Input
+              name="length"
+              size="large"
+              placeholder="length (cm)"
+              onChange={this.handleInput}
+              value={this.state.length}
+            />
+            <Input
+              name="width"
+              size="large"
+              placeholder="width (cm)"
+              onChange={this.handleInput}
+              value={this.state.width}
+            />
+            <Input
+              name="height"
+              size="large"
+              placeholder="height (cm)"
+              onChange={this.handleInput}
+              value={this.state.height}
+            />
+          </Col>
+        </Row>
+
+        <div className="divider" />
+
+        <Row type="flex" gutter={16} style={{ padding: '32px 32px 0 32px' }}>
+          <span>Measured Reverberation Times - RT60 (s)</span>
+        </Row>
+
+        <Row type="flex" gutter={16} style={{ padding: '32px 32px 0 32px' }}>
+          <VictoryChart
+            height={200}
+            width={400}
+          >
+            <VictoryAxis
+              tickValues={FrequencyDomain}
+              scale={{ x: 'log', y: 'linear' }}
+            />
+            <VictoryAxis dependentAxis />
+            <VictoryArea
+              // interpolation="natural"
+              x="frequency"
+              y="RT60"
+              domain={{ x: [63, 8000] }}
+              data={measuredRT60Data}
+            />
+          </VictoryChart>
+        </Row>
+
+        <Row type="flex" gutter={16} style={{ textAlign: 'center' }}>
+          <Col span={8}> </Col>
+          {FrequencyDomain.map(hz => (
+            <Col span={2} key={hz}>
+              <strong>{hz}</strong>Hz
+            </Col>
+          ))}
+        </Row>
+
+        <Row type="flex" gutter={16} style={{ paddingTop: '32px' }}>
+          <Col span={8} style={{ paddingTop: '8px' }}>
+            <strong>Measured Reverberation Times</strong> (RT60 / s)
+          </Col>
+          {FrequencyDomain.map(hz => (
+            <Col span={2} key={hz}>
+              <Input
+                name={hz}
+                size="large"
+                placeholder={hz}
+                onChange={this.handleMeasuredRT60Input}
+                value={this.state.measuredRT60[hz]}
+                style={{ textAlign: 'center' }}
+              />
+            </Col>
+          ))}
+        </Row>
+
+        <Row type="flex" gutter={16}>
+          <Col span={8} className="grid-first-col">
+            <strong>Equivalent Absorption Areas</strong> &nbsp; (Aeq / m<sup>2</sup>)
+          </Col>
+          {FrequencyDomain.map(hz => (
+            <Col span={2} key={hz} className="grid-col">
+              {A_eqs[hz] || '-'}
+            </Col>
+          ))}
+        </Row>
+
+        <Row type="flex" gutter={16}>
+          <Col span={8} className="grid-first-col">
+            <strong>Additional Absorption Areas</strong> &nbsp; (Aeq / m<sup>2</sup>)
+          </Col>
+          {FrequencyDomain.map(hz => (
+            <Col span={2} key={hz} className="grid-col">
+              {A_adds[hz] || '-'}
+            </Col>
+          ))}
+        </Row>
+
+        <div className="divider" />
+
+        <Row type="flex" gutter={16} style={{ padding: '32px' }}>
+          <Col span={6}>
+            Total Surface Area: {surfaceAreas ? surfaceAreas.total : 0} m<sup>2</sup>
+          </Col>
+          <Col span={6}>
+            Volume: {volume || 0} m<sup>3</sup>
+          </Col>
+        </Row>
+
+        <div className="divider" />
+
+        <Row type="flex" gutter={16}>
+          <Col span={8} className="grid-first-col">
+            <strong>Room Usage</strong>&nbsp;(DIN 18041)
+          </Col>
+          <Col span={16}>
+            <Radio.Group size="large" defaultValue="a" buttonStyle="solid">
+              <Radio.Button value="A1">Music</Radio.Button>
+              <Radio.Button value="A2">Speech / Presentation</Radio.Button>
+              <Radio.Button value="A3">Education / Communication</Radio.Button>
+            </Radio.Group>
+          </Col>
+        </Row>
+
+        <Row type="flex" gutter={16}>
+          <Col span={8} className="grid-first-col">
+            <strong>Target Reverberation Time</strong>
+            &nbsp;
+            (RT60 / s)
+            &nbsp;
+            <Switch
+              checkedChildren={<Icon type="edit" />}
+              name="customRT60Target"
+              checked={this.state.customRT60Target}
+              onChange={this.handleCustomRT60Target}
+            />
+          </Col>
+          <Col span={16}>
+            <InputNumber
+              name="RT60Target"
+              size="large"
+              step={.05}
+              onChange={this.handleCustomRT60TargetInput}
+              value={this.state['RT60Target']}
+              disabled={!this.state.customRT60Target}
+            />
+          </Col>
+        </Row>
+
+        <div className="divider" />
+
+        <Row type="flex" gutter={16} style={{ padding: '32px 32px 0 32px' }}>
+          <span>Measured Reverberation Times - RT60 (s)</span>
+        </Row>
+
+        <Row type="flex" gutter={16} style={{ padding: '32px' }}>
+          {FrequencyDomain.map(hz => (
+            <Col span={3} key={hz}>
+              {this.state[hz+'-A-eq']}
+            </Col>
+          ))}
+        </Row>
+
+        <Row type="flex" gutter={16} style={{ padding: '32px' }}>
+          <Col span={6}>
+            Average
+          </Col>
+          <Col span={6}>
+          </Col>
+        </Row>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
